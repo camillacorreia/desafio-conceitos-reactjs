e61ef4b301e41477ea11e577850457bc9a3b8dbc
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-29 12:39:35 -0300

    feat: listando repositórios

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
 
   useEffect(() => {
     api.get('repositories').then(response => {
-      console.log(response)
+      setRepositories(response.data);
     })
   }, []);
 
   async function handleAddRepository() {
-  // TODO
+    // TODO
   }
 
   async function handleRemoveRepository(id) {
@@ -23,13 +23,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
