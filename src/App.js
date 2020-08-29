4abfe6c147f3295e1ef8ac5a2856f44f074c1c5a
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-29 12:29:02 -0300

    feat: conexão com api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response)
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+  // TODO
   }
 
   async function handleRemoveRepository(id) {
